c41f60359acbd06d58b7b854e1b36b0cc8be251a
FamousT <tobby70000@gmail.com>
2020-07-01 16:48:34 +0100

    contact page

--- js/main.js
+++ js/main.js
@@ -234,8 +234,8 @@ anime.timeline({loop: true})
     targets: '.ml3 .letter',
     opacity: [0,1],
     easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i+1)
+    duration: 250,
+    delay: (el, i) => 70 * (i+1)
   }).add({
     targets: '.ml3',
     opacity: 0,
